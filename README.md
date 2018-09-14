04d62499355265a71761b2a10bad6bfbcec99e71
Nicholas Kristoffersen <nick.kristoffersen@retriever.no>
2018-09-14 12:49:46 +0200

    clean up example test and add documentation links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## write integration tests with jest
 
+test https://api.met.no/weatherapi/documentation integration. Test for every HTTP status codes as possible.
+
 see https://jestjs.io/docs/en/asynchronous for more info
--- integration.test.js
+++ integration.test.js
@@ -1,11 +1,4 @@
 import axios from "axios";
-import convert from "xml-js";
-
-axios
-  .get("https://api.met.no/weatherapi/locationforecast/1.9/?lat=60.10&lon=9.58")
-  .then(response =>
-    console.log(convert.xml2json(response.data, { compact: false, spaces: 2 }))
-  );
 
 test("Request api.met.no and get 200 status", () => {
   expect.assertions(1);
